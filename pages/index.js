c6f16f1175d447c00991532a6f746bfdb5242bc2
Leonardo Ratzlaff <leoratzlaff@gmail.com>
2022-03-29 22:29:41 -0300

    Chakra

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Header from './Header';
 import Flag from './Flag';
 import exportAsImage from './utils/exportAsImage';
 
+import { Button, FormControl, FormLabel, Heading, HStack, Input, RadioGroup, Radio, Stack } from '@chakra-ui/react'
+
 function App() {
 
   const exportRef = useRef();
@@ -20,13 +22,24 @@ function App() {
           <Flag text={text} />
         </div>
         <div>
-          <h1>Make your own flag</h1>
-          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+          <Heading mb={8} >Customize sua bandeira</Heading>
+          <Stack direction='column' spacing={4} align='left'>
 
+          <FormControl>
+            <FormLabel htmlFor='texto'>Texto</FormLabel>
+            <Input id="texto" value={text} onChange={e => setText(e.target.value)} placeholder='ORDEM E PROGRESSO' style={{textTransform: 'uppercase'}} />
+          </FormControl>
 
-          <button onClick={() => exportAsImage(exportRef.current, "Bandeirola")}>
-          Capture Image
-          </button>
+          <RadioGroup defaultValue='1'>
+            <FormLabel htmlFor='texto'>Estilo</FormLabel>
+            <HStack spacing='24px'>
+              <Radio value='1'>1x1 + zoom</Radio>
+              <Radio value='2'>Oficial (10x7)</Radio>
+            </HStack>
+          </RadioGroup>
+          <Button onClick={() => exportAsImage(exportRef.current, "Bandeirola")} colorScheme='green'>Baixar imagem</Button>
+          </Stack>
+
         </div>
       </section>
     </main>
