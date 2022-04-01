fa96cfc3f922cb428e37270e5f665703340c661d
Leonardo Ratzlaff <leoratzlaff@gmail.com>
2022-03-31 22:47:57 -0300

    Format options + styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react';
 import Header from './Header';
 import Flag from './Flag';
+import RadioButton from './RadioButton';
 import exportAsImage from './utils/exportAsImage';
 
 function App() {
@@ -8,6 +9,14 @@ function App() {
   const exportRef = useRef();
 
   const [text, setText] = useState("TODO DIA UM 7 A 1 DIFERENTE");
+  const [format, setFormat] = useState(1);
+
+  const handleFormat1 = () => {
+    setFormat(1);
+  };
+  const handleFormat2 = () => {
+    setFormat(2);
+  };
 
   return (
     <main>
@@ -19,14 +28,22 @@ function App() {
         <div className="flag">
           <Flag text={text} />
         </div>
-        <div>
-          <h1>Make your own flag</h1>
+        <div className="controls">
+          <h1>Customize sua bandeira</h1>
+          <label className="label">Texto</label>
           <input type="text" value={text} onChange={e => setText(e.target.value)} />
-
-
-          <button onClick={() => exportAsImage(exportRef.current, "Bandeirola")}>
-          Capture Image
-          </button>
+          <span className="label">Formato</span>
+          <RadioButton
+            label="1x1 + zoom"
+            value={format === 1}
+            onChange={handleFormat1}
+          />
+          <RadioButton
+            label="Oficial"
+            value={format === 2}
+            onChange={handleFormat2}
+          />
+          <button onClick={() => exportAsImage(exportRef.current, "Bandeirola", format)}>Baixar imagem</button>
         </div>
       </section>
     </main>
--- pages/utils/exportAsImage.js
+++ pages/utils/exportAsImage.js
@@ -1,8 +1,27 @@
 
   import html2canvas from "html2canvas";
   
-  const exportAsImage = async (element, imageFileName) => {
-    const canvas = await html2canvas(element);
+  const exportAsImage = async (element, imageFileName, format) => {
+
+    const image_w = 1080;
+    const image_h = 756;
+
+    let options = {
+      scale: 4,
+      x: (image_w/2)/2,
+      y: (image_h-(image_w/2))/2,
+      width: image_w/2,
+      height: image_w/2
+    }
+    if(format == 2){
+      options = {
+        scale: 4,
+        width: image_w,
+        height: image_h
+      }
+    }
+
+    const canvas = await html2canvas(element, options);
     const image = canvas.toDataURL("image/png", 1.0);
     downloadImage(image, imageFileName);
   };
